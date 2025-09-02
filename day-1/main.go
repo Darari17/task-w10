aa820dca17ded0bceb96b2358304e7b9700b14d0
farid <faridramadhan59@gmail.com>
2025-09-02 17:06:24 +0700

    feat: add new struct request

--- day-1/main.go
+++ day-1/main.go
@@ -12,21 +12,44 @@ type User struct {
 	Password string
 }
 
-type Request struct {
+type RequestLogin struct {
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type RequestRegister struct {
 	Email    string `json:"email" binding:"required,email,max=100"`
-	Password string `json:"password" binding:"required,min=6"`
+	Password string `json:"password" binding:"required,min=8"`
 }
 
 type Response struct {
-	IsSuccess bool   `json:"is_success"`
-	Email     string `json:"email"`
+	Message string `json:"message,omitempty"`
+	Error   string `json:"error,omitempty"`
 }
 
 func main() {
 	r := gin.Default()
 
+	users := []User{
+		{
+			Id:       1,
+			Email:    "farid@mail.com",
+			Password: "farid@12345",
+		},
+		{
+			Id:       2,
+			Email:    "rhamadhan@mail.com",
+			Password: "rhamadhan@12345",
+		},
+		{
+			Id:       3,
+			Email:    "darari@mail.com",
+			Password: "darari@12345",
+		},
+	}
+
 	r.POST("/login", func(ctx *gin.Context) {
-		input := Request{}
+		input := RequestLogin{}
 		if err := ctx.ShouldBindJSON(&input); err != nil {
 			ctx.JSON(http.StatusInternalServerError, gin.H{
 				"error": err.Error(),
@@ -34,14 +57,24 @@ func main() {
 			return
 		}
 
-		ctx.JSON(http.StatusOK, Response{
-			IsSuccess: true,
-			Email:     input.Email,
+		for _, v := range users {
+			if input.Email == v.Email {
+				if input.Password == v.Password {
+					ctx.JSON(http.StatusOK, Response{
+						Message: "Login Berhasil",
+					})
+					return
+				}
+			}
+		}
+
+		ctx.JSON(http.StatusBadRequest, Response{
+			Error: "Passowrd atau Email Salah",
 		})
 	})
 
 	r.POST("/register", func(ctx *gin.Context) {
-		input := Request{}
+		input := RequestRegister{}
 		if err := ctx.ShouldBindJSON(&input); err != nil {
 			ctx.JSON(http.StatusInternalServerError, gin.H{
 				"error": err.Error(),
@@ -50,8 +83,7 @@ func main() {
 		}
 
 		ctx.JSON(http.StatusCreated, Response{
-			IsSuccess: true,
-			Email:     input.Email,
+			Message: "Register Berhasil",
 		})
 	})
 
